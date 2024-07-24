8a078abdd4f5b7d053cf616bd7cc112605c75dd3
raghavsingh05 <raghavsingh47336@gmail.com>
2024-07-24 19:59:18 +0530

    added new video card code

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,7 +1,23 @@
 
 
-function VideoCard(){
-    return <div>
-        Hi from video card
+export function VideoCard(props: any) {
+    return <div className="p-2">
+        <img className="rounded-xl" src={props.image} alt="" />
+        <div className="grid grid-cols-12 pt-2">
+            <div className="col-span-1 pr- h-10 w-10 ">
+                <img className="rounded-full " src={props.thumbimage} alt="" />
+            </div>
+            <div className="col-span-6">
+                <div className=" text-sm">
+                    {props.title}
+                </div>
+                <div className=" text-gray-400 text-xs">
+                    {props.author}
+                </div>
+                <div className=" text-gray-400 text-xs">
+                    {props.views} | {props.timestamp}
+                </div>
+            </div>
+        </div>
     </div>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import {VideoCard} from "../components/VideoCard"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      hi there
+      <VideoCard 
+      title ={"how to learn MERN in 90 days | Code with me"} 
+      author={"Raghav-live"} 
+      views={"10M"} 
+      timestamp={"10 days ago"} 
+      image ={"photo1.jpg"} 
+      thumbimage={"thumb.jpg"} />
     </div>
   );
 }
